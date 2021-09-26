8420214c789fe5a3e5badd026f66049a73c1d0c3
mijanrony12e <mijaninfo12@gmail.comm>
2021-09-26 06:09:40 +0600

    finishied

--- src/components/SingleMember/SingleMember.js
+++ src/components/SingleMember/SingleMember.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SingleMember.css'
-import { faUser} from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const SingleMember = (props) => {
 
@@ -13,34 +13,38 @@ const SingleMember = (props) => {
         
             //   i am showing every member
             <div className="col-md-4">
-                <div className="member">
+                    <div className="member">
                             <div className="member">
                            <img src={img} alt="" />
                    </div>
-            <div>
-                <h6>Name:- { name}</h6>           
-                    <table>    
-                    <tr>
-                        <td>Owner:-</td>
-                        <td>{ owner}</td>
-                    </tr>
-                    <tr>
-                        <td>Designation:-</td>
-                        <td>{ designation}</td>
-                    </tr>
-                    <tr>
-                        <td>Age:-</td>
-                        <td>{ age}</td>
-                    </tr>
-                    <tr>
-                        <td>Salary:-(TK)
-                        </td>
-                        <td>{ salary}</td>
-                    </tr>
-                    </table>
+                <div>
+                        <h5>Name:- { name}</h5>           
+                            <table>    
+                            <tr>
+                                <td>Owner:-</td>
+                                <td>{ owner}</td>
+                            </tr>
+                            <tr>
+                                <td>Designation:-</td>
+                                <td>{ designation}</td>
+                            </tr>
+                            <tr>
+                                <td>Age:-</td>
+                                <td>{ age}</td>
+                            </tr>
+                            <tr>
+                                <td>Salary:-(TK)
+                                </td>
+                                <td>{ salary}</td>
+                            </tr>
+                            </table>
 
                     {/* get data */}
-                    <button onClick={ () => props.addHandler(props.member) }> <small>{ man}</small> Add Member</button>
+                    <button onClick={ () => props.addHandler(props.member) }> <small>{ man }</small> Add Member</button>
+                    <div className="icon">
+                                <i class="fab fa-facebook-square"></i>
+                               <i class="fab fa-instagram-square"></i>
+                    </div>
                    </div>
                  </div>
               </div>
